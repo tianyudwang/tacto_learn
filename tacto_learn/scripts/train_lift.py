4e3432f358cde8c286447415a4b92812469ae347
erl <danieltianyuwang@gmail.com>
2022-09-18 13:14:47 -0700

    update

--- tacto_learn/scripts/train_lift.py
+++ tacto_learn/scripts/train_lift.py
@@ -16,11 +16,18 @@ def make_env(env_cfg=None):
         default_controller=env_cfg['controller_configs']['default_controller']
     )
     env_cfg['controller_configs'] = controller_configs
+
+    # Omit object true state if using camera observation
+    if env_cfg['use_camera_obs']:
+        env_cfg['use_object_obs'] = False
+    else:
+        env_cfg['use_object_obs'] = True
+ 
     env = suite.make(**env_cfg)
     env = GymWrapper(env)
     return env
 
-def train_policy(env, policy_cfg):
+def train_policy(env, policy_cfg, exp_name):
 
     policy_kwargs = dict(
         features_extractor_class=DictExtractor,
@@ -37,20 +44,20 @@ def train_policy(env, policy_cfg):
         'MultiInputPolicy', 
         env,
         buffer_size=policy_cfg['buffer_size'], 
-        policy_kwargs=policy_kwargs
+        gradient_steps=-1,
+        policy_kwargs=policy_kwargs,
     )
 
     from stable_baselines3.common.logger import configure
-    data_path = osp.abspath(osp.join(osp.dirname(osp.realpath(__file__)), '../../logs'))
-    policy_name = policy_cfg['policy_name']
-    tmp_path = data_path + f'/{policy_name}'
+    log_path = osp.abspath(osp.join(osp.dirname(osp.realpath(__file__)), '../../logs'))
+    tmp_path = osp.join(log_path, exp_name)
 
     # set up logger
     new_logger = configure(tmp_path, ["stdout", "tensorboard"])
     policy.set_logger(new_logger)
     policy.learn(total_timesteps=policy_cfg['total_timesteps'], log_interval=4)
 
-    policy_name = policy_path + f'/{policy_name}'
+    policy_name = osp.join(policy_path, exp_name)
     policy.save(policy_name)
     return policy
 
@@ -72,19 +79,34 @@ def main():
     # Merge command line arguments with configs
     if args.seed is not None:
         cfg['seed'] = args.seed
+    print(cfg)
+
+    # Generate experiment name 
+    # algorithm + env_name + robots + controller + obs_mode
+    env_cfg, policy_cfg = cfg['env'], cfg['policy']
+    if env_cfg['use_camera_obs']:
+        obs_mode = 'visual_proprio'
+    else:
+        obs_mode = 'object_state_proprio'
+    exp_name_args = [
+        policy_cfg['policy_name'],
+        env_cfg['env_name'],
+        env_cfg['robots'],
+        env_cfg['controller_configs']['default_controller'],
+        obs_mode,
+    ]
+    exp_name = '_'.join(exp_name_args)
 
     # make env
     env = make_vec_env(
         make_env, 
-        env_kwargs=dict(env_cfg=cfg['env']), 
+        env_kwargs=dict(env_cfg=env_cfg), 
         vec_env_cls=SubprocVecEnv, 
         n_envs=8
     )
-    # env = SubprocVecEnv([make_env(cfg['env']) for])
-    # env = make_env(cfg['env'])
 
     # train policy
-    policy = train_policy(env, cfg['policy'])
+    policy = train_policy(env, policy_cfg, exp_name)
 
 if __name__ == '__main__':
     main()
