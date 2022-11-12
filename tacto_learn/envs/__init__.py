b219d698b6c33c7a10b02a00e09a061a0da16c24
Tianyu Wang <danieltianyuwang@gmail.com>
2022-11-11 17:03:12 -0800

    update

--- tacto_learn/envs/__init__.py
+++
@@ -1,7 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id="sawyer-gripper-v0", 
-    entry_point="tacto_learn.envs.sawyer_gripper_env:make_sawyer_gripper_env",
-    max_episode_steps=250,
-)
